501572d075dbf6794ab904db12c3fc3f37364a2d
Jon Reid <jon@qualitycoding.org>
2025-06-14 14:00:14 -0700

    . d Add file comments to demo and remove template test

--- Demo/Sources/Demo/AssertEqual.swift
+++ Demo/Sources/Demo/AssertEqual.swift
@@ -1,3 +1,7 @@
+// FailKit by Jon Reid, https://qualitycoding.org
+// Copyright 2025 Jonathan M. Reid. https://github.com/jonreid/FailKit/blob/main/LICENSE.txt
+// SPDX-License-Identifier: MIT
+
 import FailKit
 
 public func assertEqual<T: Equatable>(
