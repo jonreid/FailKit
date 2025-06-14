02015e88007b5d86286c27c919999e473adaefbe
Jon Reid <jon@qualitycoding.org>
2025-06-13 21:34:12 -0700

    ^ f Add public initializer for SourceLocation

--- Sources/FailKit/Failing/SourceLocation.swift
+++ Sources/FailKit/Failing/SourceLocation.swift
@@ -12,6 +12,13 @@ public struct SourceLocation {
     public let line: UInt
     public let column: UInt
 
+    public init(fileID: String, filePath: StaticString, line: UInt, column: UInt) {
+        self.fileID = fileID
+        self.filePath = filePath
+        self.line = line
+        self.column = column
+    }
+
 #if canImport(Testing)
     public func toTestingSourceLocation() -> Testing.SourceLocation {
         Testing.SourceLocation(fileID: fileID, filePath: "\(filePath)", line: Int(line), column: Int(column))
