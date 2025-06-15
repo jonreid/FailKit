1fa407c176006620c56876eaee411349f8be1a60
Jon Reid <jon@qualitycoding.org>
2025-06-14 17:05:22 -0700

    . e Add platforms to manifest

--- Package.swift
+++ Package.swift
@@ -4,6 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "FailKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1)
+    ],
     products: [
         .library(
             name: "FailKit",
